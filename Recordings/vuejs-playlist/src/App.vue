cbcd838a3c30fcf44b16d1d89d887120e3ccaaae
Jose Torreblanca <josewhitetower@gmail.com>
2017-08-19 23:46:56 +0200

    Dynamics Components

--- Recordings/vuejs-playlist/src/App.vue
+++ Recordings/vuejs-playlist/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div >
-      <form-helper>
-       <div slot='form-header'>
-         <h3>This is the title of the form</h3>
-         <p>This is information about the form</p>
-       </div>
-       <div slot='form-fields'>
-         <input type='text' placeholder="name" required>
-         <input type='password' placeholder="password" required>
-       </div>
-       <div slot='form-controls'>
-         <button v-on:click='handleSubmit()'>Submit</button>
-       </div>
-      </form-helper> 
+    <keep-alive> <!-- to kepp the data alive when you switch components-->
+      <component v-bind:is="component"></component> 
+    </keep-alive>
+      <button v-on:click="component='form-one'">Show Form one</button>
+      <button v-on:click="component='form-two'">Show Form two</button>
       
   </div>    
 </template>
 
 <script>
-import formHelper from './components/formHelper.vue';
+import formOne from './components/formOne.vue';
+import formTwo from './components/formTwo.vue';
 
 export default {
   components:{
-   'form-helper':formHelper
+   'form-one':formOne,
+   'form-two':formTwo
    },   
   data () {
    return{
-      title:'I am a slot title' // The dynamic data must be defined where the component where the slot is created
+      component:'form-one' // The dynamic data must be defined where the component where the slot is created
    }
   },
   methods:{
