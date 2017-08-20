fe3d50dfc9f0ee4ef85d4f1dfef30b4614de603c
Jose Torreblanca <josewhitetower@gmail.com>
2017-08-21 01:00:12 +0200

    Making GET requests

--- Recordings/vuejs-playlist/src/App.vue
+++ Recordings/vuejs-playlist/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div >
     <add-blog></add-blog>
+    <show-blog></show-blog>
   </div>    
 </template>
 
 <script>
 import addBlog from './components/addBlog.vue';
+import showBlog from './components/showBlog.vue';
+
 export default {
   components:{
-  'add-blog':addBlog
+  'add-blog':addBlog,
+  'show-blog':showBlog
   },
   data () {
    return{
