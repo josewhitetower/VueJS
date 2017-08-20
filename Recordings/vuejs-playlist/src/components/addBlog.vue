482a16421546f347e4cc41abc201f4330147abc0
Jose Torreblanca <josewhitetower@gmail.com>
2017-08-21 00:14:54 +0200

    Http Request

--- Recordings/vuejs-playlist/src/components/addBlog.vue
+++ Recordings/vuejs-playlist/src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id ="add-blog">
     <h2>Add a New Blog</h2>
-    <form>
+    <form v-if="!submitted"> 
        <label>Blog Title</label>
        <input type="text" v-model.lazy="blog.title" required>
        <label>Blog Content</label>
@@ -20,8 +20,11 @@
         <option disabled value="">Please select one</option>
         <option v-for="author in authors" :key="author">{{author}}</option>
     </select>
+    <button v-on:click.prevent="post">Add Post</button>
     </form>
-
+   <div v-if="submitted">
+       <h3>Thanks for adding your post</h3>
+   </div>
     <div id="preview">
         <h3>Preview Blog</h3>
         <p>Blog Title: {{blog.title}}</p>
@@ -49,11 +52,21 @@ export default {
         categories:[],
         author:""
        } ,
-       authors:['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']     
+       authors:['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']  ,
+       submitted:false   
    }
   },
   methods:{
-    
+    post: function(){
+        this.$http.post("https://jsonplaceholder.typicode.com/posts",{
+            title:this.blog.title,
+            body:this.blog.content,
+            userId:1
+        }).then(function(data){
+            console.log(data);
+            this.submitted=true; 
+        });
+    }
   }
 }
 </script>
--- Recordings/vuejs-playlist/src/main.js
+++ Recordings/vuejs-playlist/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 
 export const bus = new Vue();
 // import Ninjas from './Ninjas.vue'
-
+Vue.use(VueResource);
 
 // Vue.component('ninjas', Ninjas);
 
